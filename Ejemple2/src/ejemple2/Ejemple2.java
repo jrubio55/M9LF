bfc21b32f82c9464c35ee0fdf88d6282b8fa9b8a
jrubio55 <jrubio55@xtec.cat>
2016-09-16 20:00:11 +0200

    Update Ejemple2.java

--- Ejemple2/src/ejemple2/Ejemple2.java
+++ Ejemple2/src/ejemple2/Ejemple2.java
@@ -17,6 +17,7 @@ public class Ejemple2 {
     public static void main(String[] args) {
         // TODO code application logic here
         String lan = "Hola";
+        System.out.println(lan);
     }
     
 }
