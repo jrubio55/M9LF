77afe4bf3cd9c82dcb47e5406d3d24bdadc61cf9
Jorge <Jorge@DESKTOP-RTQUE07>
2016-09-16 19:36:08 +0200

    Merge origin/master
    Conflicts:
    	JavaApplication4/src/javaapplication4/JavaApplication4.java

---
+++ Ejemple2/src/ejemple2/Ejemple2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemple2;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Ejemple2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String lan = "Hola";
+    }
+    
+}
