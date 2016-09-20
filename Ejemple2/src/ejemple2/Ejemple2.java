10789097a249c5479ca120f35c2263f1e1080324
Jorge <Jorge@DESKTOP-RTQUE07>
2016-09-20 21:07:14 +0200

    Merge commit 'bfc21b32f82c9464c35ee0fdf88d6282b8fa9b8a'
    Conflicts:
    	Ejemple2/src/ejemple2/Ejemple2.java

--- Ejemple2/src/ejemple2/Ejemple2.java
+++ Ejemple2/src/ejemple2/Ejemple2.java
@@ -17,7 +17,7 @@ public class Ejemple2 {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        String lang = "Solucionado";
+        String lang = "Solucionado error3";
         System.out.println(lang);
 
     }
