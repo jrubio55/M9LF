a943f50d9d7a76fdd51d7bd5fd287124696b18e5
Jorge <Jorge@DESKTOP-RTQUE07>
2016-09-23 18:23:34 +0200

    Merge commit 'bfc21b32f82c9464c35ee0fdf88d6282b8fa9b8a'
    Conflicts:
    	Ejemple2/src/ejemple2/Ejemple2.java

--- ThreadPoolExecutor/src/threadpoolexecutor/ThreadPoolExecutor.java
+++ ThreadPoolExecutor/src/threadpoolexecutor/ThreadPoolExecutor.java
@@ -1,15 +1,14 @@
-
 package threadpoolexecutor;
 
 public class ThreadPoolExecutor {
 
     public static void main(String[] args) {
-        Servidor servidor = new Servidor(); 
-        for (int i = 0; i < 10; i++) {  
-        Tarea tarea = new Tarea("Tarea " + i);  
-        servidor.ejecutaTarea(tarea);  
-     }  
-     servidor.terminaServidor();  
+        Servidor servidor = new Servidor();
+        for (int i = 0; i < 50; i++) {
+            Tarea tarea = new Tarea("Tarea " + i);
+            servidor.ejecutaTarea(tarea);
+        }
+        servidor.terminaServidor();
     }
-    
+
 }
