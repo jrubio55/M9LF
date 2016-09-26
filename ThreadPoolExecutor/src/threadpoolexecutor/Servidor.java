e636341370985aa4c71826b31da40d462b936a9b
Jorge <Jorge@DESKTOP-RTQUE07>
2016-09-26 10:49:03 +0200

    Merge commit 'bfc21b32f82c9464c35ee0fdf88d6282b8fa9b8a'
    Conflicts:
    	Ejemple2/src/ejemple2/Ejemple2.java

--- ThreadPoolExecutor/src/threadpoolexecutor/Servidor.java
+++ ThreadPoolExecutor/src/threadpoolexecutor/Servidor.java
@@ -9,12 +9,12 @@ public class Servidor {
     //private final ExecutorService executor;
     
     public Servidor() {  
-        executor = (ThreadPoolExecutor)Executors.newCachedThreadPool(); //Crea el objeto executor o un pool de threads  
-        //executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5); //Crea el objeto executor con 5 threads
+        //executor = (ThreadPoolExecutor)Executors.newCachedThreadPool(); //Crea el objeto executor o un pool de threads  
+        executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5); //Crea el objeto executor con 5 threads
         //executor = (ExecutorService) Executors.newSingleThreadExecutor(); //Crea el objeto executor con 1 thread
-    }  
+    } 
     public void ejecutaTarea(Tarea tarea) {  
-        executor.execute(tarea);// Ejecuta una tarea 
+       executor.execute(tarea);// Ejecuta una tarea 
     }  
     public void terminaServidor () {  
         executor.shutdown();// Este metodo termina el executor
--- ThreadPoolExecutor/src/threadpoolexecutor/ThreadPoolExecutor.java
+++ ThreadPoolExecutor/src/threadpoolexecutor/ThreadPoolExecutor.java
@@ -4,7 +4,7 @@ public class ThreadPoolExecutor {
 
     public static void main(String[] args) {
         Servidor servidor = new Servidor();
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 10; i++) {
             Tarea tarea = new Tarea("Tarea " + i);
             servidor.ejecutaTarea(tarea);
         }
